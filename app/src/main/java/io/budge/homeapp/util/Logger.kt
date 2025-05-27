523bd5e1eca0f891085363fd08cbc72d0cce9f05
Jeremiah Adeleye <jerryb.adeleye@gmail.com>
2025-05-27 17:14:03 +0100

    HomeApp: Cleanup

--- app/src/main/java/io/budge/homeapp/util/Logger.kt
+++ app/src/main/java/io/budge/homeapp/util/Logger.kt
@@ -7,12 +7,4 @@ object Logger {
     fun v(tag: String, message: String) {
         Log.v(tag, message)
     }
-
-    fun d(tag: String, message: String) {
-        Log.d(tag, message)
-    }
-
-    fun e(tag: String, message: String, throwable: Throwable? = null) {
-        Log.e(tag, message, throwable)
-    }
 }
